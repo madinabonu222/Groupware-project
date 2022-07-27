5b236e36c9109d092c056199b262099fd7eebc4b
madinabonu <misssonmi25@gmail.com>
2022-07-27 15:20:48 +0900

    update new

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -7,16 +7,16 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate property plans"
     _order = "sequence"
-    def create (self,values):
-        override_create = super(EstateProperty,self).create(values)
-        return override_create
+    # def create (self,values):
+    #     button_create = super(EstateProperty,self).create(values)
+    #     return button_create
     
     # name = fields.Char('Property Name', required=True, translate=True)
-    name = fields.Char(default="Unknown")
+    title = fields.Char(default="Unkown")
     last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
-    description = fields.Text('Text')
+    description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    data_availability = fields.Date('Data')
+    data_availability = fields.Date('Available From')
     expected_price = fields.Float('Expected Price')
     selling_price = fields.Float('Selling Price')
     bedrooms = fields.Integer('Bedrooms')
@@ -26,12 +26,15 @@ class EstateProperty(models.Model):
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection = [('north','North'),('south','South'),('east','East'),('west','West')],
         help = "Orientation is used to separate geographical location"
     )
     sequence = fields.Integer('Sequence')
-    active = fields.Boolean(active=False)
-    state = fields.Boolean(default="New")
+
+    # active = fields.Boolean(active=False)
+    active = fields.Boolean('Active', default=True)
+    status = fields.Boolean(default="New")
+    # state = fields.Boolean(default="New")
 
 
